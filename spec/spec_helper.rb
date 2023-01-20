785f4cdf486af0419040c6ee122e5f665570a12d
Joel Turkel <jturkel@salsify.com>
2023-01-20 14:55:20 -0500

    Use standard test DB setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ require 'database_cleaner'
 require 'delayed_job_heartbeat_plugin'
 require 'yaml'
 require 'timecop'
+require 'pg'
 
 spec_dir = File.dirname(__FILE__)
 Dir["#{spec_dir}/support/**/*.rb"].sort.each { |f| require f }
@@ -31,34 +32,51 @@ Delayed::Worker.logger.level = Logger::DEBUG
 ActiveRecord::Base.logger = Delayed::Worker.logger
 ActiveRecord::Migration.verbose = false
 
-database_name = 'delayed_job_heartbeat_plugin_test'
-database_host = ENV.fetch('PGHOST', 'localhost')
-database_port = ENV.fetch('PGPORT', 5432)
+database_host = ENV.fetch('DB_HOST', 'localhost')
+database_port = ENV.fetch('DB_PORT', 5432)
+database_user = ENV.fetch('DB_USER', 'postgres')
+database_password = ENV.fetch('DB_PASSWORD', 'password')
+database_url = "postgres://#{database_user}:#{database_password}@#{database_host}:#{database_port}"
+admin_database_name = "/#{ENV['ADMIN_DB_NAME']}" if ENV['ADMIN_DB_NAME'].present?
+
+DATABASE_NAME = 'delayed_job_heartbeat_plugin_test'
+
+def setup_test_database(pg_conn, database_name)
+  pg_conn.exec("DROP DATABASE IF EXISTS #{database_name}")
+  pg_conn.exec("CREATE DATABASE #{database_name}")
+
+  pg_version = pg_conn.exec('SELECT version()')
+  puts "Testing with Postgres version: #{pg_version.getvalue(0, 0)}"
+  puts "Testing with ActiveRecord #{ActiveRecord::VERSION::STRING}"
+end
+
+def teardown_test_database(pg_conn, database_name)
+  pg_conn.exec("DROP DATABASE IF EXISTS #{database_name}")
+end
 
 RSpec.configure do |config|
   config.order = 'random'
 
   config.before(:suite) do
-    db_connection_args = "--host #{database_host} --port #{database_port}"
-    `dropdb #{db_connection_args} --if-exists #{database_name} 2> /dev/null`
-    `createdb #{db_connection_args} #{database_name}`
+    PG::Connection.open("#{database_url}#{admin_database_name}") do |connection|
+      setup_test_database(connection, DATABASE_NAME)
+    end
 
-    pg_version = `psql #{db_connection_args} --dbname #{database_name} --tuples-only --command "select version()";`
-    puts "Testing with Postgres version: #{pg_version.strip}"
-    puts "Testing with ActiveRecord #{ActiveRecord::VERSION::STRING}"
+    ActiveRecord::Base.establish_connection("#{database_url}/#{DATABASE_NAME}")
 
-    database_url = "postgres://#{database_host}:#{database_port}/#{database_name}"
-    puts "Using database #{database_url}"
-    ActiveRecord::Base.establish_connection(database_url)
     require 'db/schema'
-    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after(:suite) do
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    PG::Connection.open("#{database_url}#{admin_database_name}") do |connection|
+      teardown_test_database(connection, DATABASE_NAME)
+    end
   end
 
   config.before do |example|
     DatabaseCleaner.strategy = example.metadata.fetch(:cleaner_strategy, :transaction)
-  end
-
-  config.before do
     DatabaseCleaner.start
   end
 
